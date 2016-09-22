be2e7908244e79f049373e497ac63170958664b6
Adrian Salceanu <e@essenciary.com>
2016-09-22 14:33:29 +0200

    updates for Julia v0.5 compatibility (mostly related to Strings warnings)

--- src/constants.jl
+++ src/constants.jl
@@ -6,29 +6,29 @@ for name in ("DAYOFWEEK", "DAYOFWEEKABBR", "MONTH", "MONTHABBR")
     valueto = Symbol("VALUETO" * name)
     tovalue = Symbol(name * "TOVALUE")
     @eval begin
-        const $tovalue = [locale => Dict{UTF8String,Int}(
+        const $tovalue = [locale => Dict{String,Int}(
             zip(map(lowercase, values(d)), keys(d))) for (locale, d) in $valueto]
     end
 end
 
-const HMS = Dict{UTF8String,Dict{UTF8String,Symbol}}(
+const HMS = Dict{String,Dict{String,Symbol}}(
     "english" => Dict(
         "h" => :hour,   "hour" => :hour,     "hours" => :hour,
         "m" => :minute, "minute" => :minute, "minutes" => :minute,
         "s" => :second, "second" => :second, "seconds" => :second,
     )
 )
-const AMPM = Dict{UTF8String,Dict{UTF8String,Symbol}}(
+const AMPM = Dict{String,Dict{String,Symbol}}(
     "english" => Dict(
         "am" => :am, "a" => :am,
         "pm" => :pm, "p" => :pm,
     )
 )
-const JUMP = UTF8String[
+const JUMP = String[
     " ", ".", ",", ";", "-", "/", "'", "at", "on", "and", "ad", "m", "t", "of", "st",
     "nd", "rd", "th", "the",
 ]
-const PERTAIN = UTF8String["of"]
-const UTC_ZONES = UTF8String["UTC", "GMT", "Z", "z"]
+const PERTAIN = String["of"]
+const UTC_ZONES = String["UTC", "GMT", "Z", "z"]
 
 const UTC = FixedTimeZone("UTC", 0)
